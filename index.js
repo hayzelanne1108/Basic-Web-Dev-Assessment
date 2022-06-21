3c1bd33fb9919772f492c72d69d4e726db100690
hayzelanne1108 <107467609+hayzelanne1108@users.noreply.github.com>
2022-06-21 09:56:16 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+
+
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}}
+
+var orderNow = document.querySelectorAll(".btn").length;
+
+for(var i = 0; i<orderNow; i++)
+
+  document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+    alert("added to cart")
+    getTransferData("orderMe")
+  })
+
